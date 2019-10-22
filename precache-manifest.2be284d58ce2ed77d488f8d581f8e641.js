1715f2e8ae5e8b38e322174f8d2958356290f658
saumya <jsaumya988@gmail.com>
2019-10-22 20:18:44 +0530

    Updates

---
+++ precache-manifest.2be284d58ce2ed77d488f8d581f8e641.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2f20a450d820b33f4c3d14e045441f",
+    "url": "/saumyaaajain/website/index.html"
+  },
+  {
+    "revision": "aa4855e59153faa65832",
+    "url": "/saumyaaajain/website/static/css/2.1bf87fc6.chunk.css"
+  },
+  {
+    "revision": "8ed90135413a8d2e67ba",
+    "url": "/saumyaaajain/website/static/css/main.96144cbf.chunk.css"
+  },
+  {
+    "revision": "aa4855e59153faa65832",
+    "url": "/saumyaaajain/website/static/js/2.a43f3709.chunk.js"
+  },
+  {
+    "revision": "8ed90135413a8d2e67ba",
+    "url": "/saumyaaajain/website/static/js/main.b2cd2be8.chunk.js"
+  },
+  {
+    "revision": "e8fccfdd478d22d5238c",
+    "url": "/saumyaaajain/website/static/js/runtime-main.a8e90dd7.js"
+  },
+  {
+    "revision": "a9e2109f199cd4473680d659fede98cb",
+    "url": "/saumyaaajain/website/static/media/145385.a9e2109f.jpg"
+  },
+  {
+    "revision": "d8f666f33e5fc786a4c545fe9043d91c",
+    "url": "/saumyaaajain/website/static/media/203-buckets-list-extra_TALL-1536x512.d8f666f3.jpg"
+  },
+  {
+    "revision": "c7f71a70e01129c0803a7f5f33dcdfd9",
+    "url": "/saumyaaajain/website/static/media/Star+Wars49.c7f71a70.jpg"
+  },
+  {
+    "revision": "af7c07faaff0d16720c25db7990579e2",
+    "url": "/saumyaaajain/website/static/media/StarJedi-DGRW.af7c07fa.ttf"
+  },
+  {
+    "revision": "5c4f78c79a65dfa4e1926e61e881e287",
+    "url": "/saumyaaajain/website/static/media/Unknown-3.5c4f78c7.bin"
+  },
+  {
+    "revision": "3fe8fbbf3eb59372bef5500116d13aa0",
+    "url": "/saumyaaajain/website/static/media/arrow1.3fe8fbbf.svg"
+  },
+  {
+    "revision": "5180693fdeea813b69a7b64fb48176dc",
+    "url": "/saumyaaajain/website/static/media/final-trailer-tall-v3-47f-1536x512.5180693f.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/saumyaaajain/website/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "0791e4cee7383349e173cd19ddc52cb4",
+    "url": "/saumyaaajain/website/static/media/may-the-fourth-be-with-you.0791e4ce.jpg"
+  },
+  {
+    "revision": "d8e522128faa72c6bf4498c603396d6e",
+    "url": "/saumyaaajain/website/static/media/rise-of-skywalker-theatrical-poster-tall-1536x512.d8e52212.jpg"
+  },
+  {
+    "revision": "459bb51540f7ae228149fa06ecfbf3e3",
+    "url": "/saumyaaajain/website/static/media/rise-of-skywalker-tickets-tall-1536x512.459bb515.jpg"
+  }
+]);
